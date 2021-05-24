639eed6a5f475fcf1a2514b13e8c15853944d24b
Rajeshwar Kashyap <work.rjkashyap05@gmail.com>
2021-05-24 22:02:50 +0530

    Add header Icon

--- react-web/src/components/Header/index.js
+++ react-web/src/components/Header/index.js
@@ -1,19 +1,36 @@
 import React from 'react'
 import { ReactComponent as MenuIcon } from '../../global/assets/icons/menu.svg'
+import {ReactComponent as LoginIcon} from "../../global/assets/icons/enter.svg";
 import * as STYLE from '../../constants/style'
 import './Header.scss'
 
 const Header = (props) => {
 
   return (
-    <section className={`Header display-flex align-center justify-space-between ${props.className ? props.className : ''}`}>
-      <div className="display-flex align-center">
-        <MenuIcon width={STYLE.ICON_WIDTH} height={STYLE.ICON_HEIGHT}  className="cursor-pointer"/>
+    <section
+      className={`Header display-flex align-center justify-space-between box-shadow ${
+        props.className ? props.className : ""
+      }`}
+    >
+      <div className="display-flex align-center flex-1-1">
+        <MenuIcon
+          width={STYLE.ICON_WIDTH}
+          height={STYLE.ICON_HEIGHT}
+          className="cursor-pointer"
+        />
         Header
       </div>
-      <span>Login</span>
+      <div className="display-flex align-center">
+        <LoginIcon className="cursor-pointer Loginicon" />
+        Student
+      </div>
+
+      <div className="display-flex align-center">
+        <LoginIcon className="cursor-pointer Loginicon" />
+        Teacher
+      </div>
     </section>
-  )
+  );
 }
 
 export default Header
