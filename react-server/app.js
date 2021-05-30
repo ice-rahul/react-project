8d429bdb7e70841ad27ba4af6c7975601da03d07
ice-rahul <meet4g@gmail.com>
2021-05-30 23:48:50 +0530

    initiate deployment

--- react-server/app.js
+++ react-server/app.js
@@ -11,4 +11,4 @@ app.use('/graphql', graphqlHTTP({
   schema: schema,
   graphiql: true,
 }));
-app.listen(process.env.PORT || 4000, () => console.log('Now browse to localhost:4000/graphql'));
+app.listen(process.env.PORT || 4000, () => console.log('Browse to localhost:4000/graphql'));
